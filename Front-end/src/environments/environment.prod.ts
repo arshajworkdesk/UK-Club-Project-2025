1e383c61427e07e1ea8b40797346b7eeed073b4a
arshajworkdesk <arshajwork@gmail.com>
2025-11-27 22:58:42 +0530

    gateway url changed

--- Front-end/src/environments/environment.prod.ts
+++ Front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-gateway-latest-33gp.onrender.com/api'
+  apiUrl: 'https://uk-gateway-service-e9cce2ape5d2atf6.southindia-01.azurewebsites.net/api'
 };
 
